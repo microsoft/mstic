962bcdce8a055bb475e4f3053227688f7791fa79
Ashwin Patil <aspatil@microsoft.com>
2022-03-23 16:22:22 -0700

    adding git workdlows and python script

---
+++ .script/get-nordvpnservers.py
@@ -0,0 +1,18 @@
+﻿import requests
+import json
+import pandas as pd
+
+# Download Nord Server list using API
+r  = requests.get('https://api.nordvpn.com/server')
+
+#Decode byte array into string
+my_json = r.content.decode('utf8')
+
+#Convert string to JSON
+data = json.loads(my_json)
+
+# Load list of JSON records into dataframe
+df = pd.DataFrame(data)
+
+# Convert to csv
+df.to_csv('nord-servers.csv',  index=False)
